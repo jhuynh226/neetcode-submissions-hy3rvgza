e156c58ed3f0727b3fb067789d6a7435c7c310a8
John Huynh <137665975+jhuynh226@users.noreply.github.com>
2026-04-16 17:02:40 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # s_map, t_map = Counter(s), Counter(t)
+        # return s_map == t_map
+        
+        # if len(s) != len(t):
+        #     return False
+
+        # countS, countT = {}, {}
+
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        #     countT[t[i]] = 1 + countT.get(t[i], 0)
+        # for key in countS:
+        #     if countS[key] != countT.get(key, 0):
+        #         return False
+
+        # return True
+
+        return sorted(s) == sorted(t)
+
+
