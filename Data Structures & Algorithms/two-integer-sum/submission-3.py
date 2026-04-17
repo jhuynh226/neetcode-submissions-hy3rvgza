e70125581363513038c4d1cfb24dea3c950b569c
John Huynh <137665975+jhuynh226@users.noreply.github.com>
2026-04-17 19:21:06 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+
+
+        #Hash Map
+        dv_map = defaultdict(int)
+
+        for i, num in enumerate(nums):
+            dv = target - num
+            if dv in dv_map:
+                return [dv_map[dv], i]
+            else:
+                dv_map[num] = i
