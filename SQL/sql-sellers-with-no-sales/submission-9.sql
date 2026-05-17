85323095900bda946d03e3f2acc32b2462f748df
John Huynh <137665975+jhuynh226@users.noreply.github.com>
2026-05-17 18:30:19 -0400

    Add: sql-sellers-with-no-sales - submission-9

---
+++ SQL/sql-sellers-with-no-sales/submission-9.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+SELECT seller_name
+FROM seller s
+WHERE s.seller_id NOT IN (
+    SELECT seller_id
+    FROM orders
+    WHERE sale_date >= '2020-01-01' AND sale_date < '2021-01-01'
+)
+ORDER BY seller_name ASC;
